1545947019453231f63bce9eb6c5c45672fbab2d
Zeeshan Anjum <zeerox92@gmail.com>
2022-07-06 15:23:10 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Amplify, API, graphqlOperation } from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { publish } from './graphql/mutations';
+Amplify.configure(awsconfig);
+
 
 function App() {
   return (
@@ -18,8 +24,33 @@ function App() {
           Learn React
         </a>
       </header>
+      <DoMutation/>
     </div>
   );
+}
+
+class DoMutation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleRetrieveCurrentSession = this.handleRetrieveCurrentSession.bind(this);
+  }
+  handleRetrieveCurrentSession(){
+    const publishChannel = { name: "My first Channel", data: "{\"hello\":20}" };
+
+    API.graphql(graphqlOperation(publish, {input: publishChannel}))
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+  }
+
+  render() {
+    return (
+    <div className="Amplify-component">
+      <h4>Do Mutation</h4>
+      <button onClick={this.handleRetrieveCurrentSession}>Check Console</button>
+    </div>
+    );
+  }
 }
 
 export default App;
